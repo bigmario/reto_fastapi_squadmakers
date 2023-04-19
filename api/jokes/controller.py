069406a0c77655a8ab6e0128268a5cfa99b53e8b
Mario Castro <mariocastro.pva@gmail.com>
2023-04-19 15:35:58 -0400

    Endpoints para chistes completados

--- api/jokes/controller.py
+++ api/jokes/controller.py
@@ -27,15 +27,90 @@ jokes_router = APIRouter(tags=["Jokes"])
 # GET JOKES FROM https://api.chucknorris.io/ or https://icanhazdadjoke.com/api
 #################################################################################
 @jokes_router.get(
-    path="/chiste/{joke_type}",
+    path="/joke/{joke_type}",
     status_code=status.HTTP_200_OK,
-    summary="Get Jokes",
+    summary="Get a joke from https://api.chucknorris.io/ or https://icanhazdadjoke.com/api",
 )
 async def get_joke(
     joke_type: str = Path(...),
     subscription_service: JokeService = Depends(),
 ):
+    return await subscription_service.get_jokes(joke_type)
+
+
+########################
+# Save Joke on DB
+########################
+@jokes_router.post(
+    path="/joke",
+    status_code=status.HTTP_201_CREATED,
+    summary="Save a Joke on Database",
+)
+async def save_joke(
+    body: Joke = Body(...),
+    joke_service: JokeService = Depends(),
+):
+    """
+    Save Joke:
+    """
+    return await joke_service.save_joke(body)
+
+
+########################
+# GET ALL JOKES ON DB
+########################
+@jokes_router.get(
+    path="/jokes",
+    status_code=status.HTTP_200_OK,
+    summary="Get All Jokes from Database",
+    response_model=Page[Joke],
+    response_model_exclude_unset=True,
+)
+async def get_all_jokes(
+    subscription_service: JokeService = Depends(),
+) -> Page[Joke]:
     try:
-        return await subscription_service.get_jokes(joke_type)
+        subscriptions = await subscription_service.get_all_jokes()
+        return paginate(subscriptions)
     except Exception as e:
         return f"An exception occurred: {e}"
+
+
+########################
+# UPDATE Joke on DB
+########################
+@jokes_router.patch(
+    path="/joke/{id}",
+    status_code=status.HTTP_200_OK,
+    summary="Update a Joke on Database",
+)
+async def update_joke(
+    number: PydanticObjectId = Path(...),
+    body: Joke = Body(...),
+    joke_service: JokeService = Depends(),
+):
+    """
+    UPdate Joke:
+    """
+    return await joke_service.update_joke(number, body)
+
+
+#############################
+# DELETE JOKE FROM DB BY ID
+#############################
+@jokes_router.delete(
+    path="/joke/{number}",
+    status_code=status.HTTP_200_OK,
+    summary="Delete One Joke By Number",
+    response_model_exclude_unset=True,
+)
+async def delete_joke(
+    number: PydanticObjectId = Path(...),
+    joke_service: JokeService = Depends(),
+) -> dict:
+    try:
+        return await joke_service.delete_joke(number)
+    except Exception:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"Joke not found!"
+        )
--- api/jokes/service/joke_service.py
+++ api/jokes/service/joke_service.py
@@ -12,20 +12,64 @@ from api.jokes.schemas.jokes import Joke
 
 class JokeService:
     def __fetch_joke(self, url):
-        joke = requests.get(url)
-        serialized_joke = json.loads(joke.text)
-        return JSONResponse(
-            {"joke": serialized_joke},
-            status_code=status.HTTP_200_OK,
-        )
+        headers = {"Accept": "application/json"}
+        joke = requests.get(url, headers=headers)
+        return joke.json()
 
     async def get_jokes(self, joke_type: str):
         if joke_type == "Chuck":
-            return self.__fetch_joke("https://api.chucknorris.io/jokes/random")
+            joke = self.__fetch_joke("https://api.chucknorris.io/jokes/random")
+            return JSONResponse(
+                {"joke": joke["value"]},
+                status_code=status.HTTP_200_OK,
+            )
+
         elif joke_type == "Dad":
-            return self.__fetch_joke("https://icanhazdadjoke.com/")
+            joke = self.__fetch_joke("https://icanhazdadjoke.com")
+            return JSONResponse(
+                {"joke": joke["joke"]},
+                status_code=status.HTTP_200_OK,
+            )
         else:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail='El chiste debe ser de tipo "Chuck" o "Dad"',
             )
+
+    async def save_joke(self, body: Joke = Body(...)):
+        existing_joke = await Joke.find_one(Joke.joke == body.joke)
+
+        if not existing_joke:
+            new_joke = await body.create()
+            return JSONResponse(
+                {"Message": "Joke successfully stored"},
+                status_code=status.HTTP_200_OK,
+            )
+        else:
+            return JSONResponse(
+                {"Message": "This joke is already registered"},
+                status_code=status.HTTP_400_BAD_REQUEST,
+            )
+
+    async def get_all_jokes(self) -> Page[Joke]:
+        return await Joke.find_all().to_list()
+
+    async def update_joke(self, id: PydanticObjectId, body: Joke = Body(...)):
+        update_joke = await Joke.find_one(Joke.id == id)
+        if update_joke:
+            update_joke.joke = body.joke
+            await update_joke.save()
+            return JSONResponse(
+                {"Message": "Joke successfully updated"},
+                status_code=status.HTTP_200_OK,
+            )
+        else:
+            return JSONResponse(
+                {"Message": "ID not registered!!"},
+                status_code=status.HTTP_400_BAD_REQUEST,
+            )
+
+    async def delete_joke(self, id: PydanticObjectId) -> dict:
+        joke = await Joke.get(id)
+        await joke.delete()
+        return {"message": "Joke successfully deleted"}
