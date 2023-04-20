13d258be828a2ed08a61cfb2e8c3844fd2a11f53
Mario Castro <mariocastro.pva@gmail.com>
2023-04-20 10:26:21 -0400

    intradocumentation for tests

--- api/test_main.py
+++ api/test_main.py
@@ -6,22 +6,34 @@ client = TestClient(app)
 
 
 def test_get_joke():
+    """
+    Assert status code 200 in fetching jokes
+    """
     response = client.get("/joke", headers={"Accept": "application/json"})
     assert response.status_code == 200
 
 
 def test_get_joke_wrong_type():
+    """
+    Assert bad type of joke
+    """
     response = client.get("/joke/chick", headers={"Accept": "application/json"})
     assert response.status_code == 400
     assert response.json() == {"detail": 'Joke type must be "Chuck" o "Dad"'}
 
 
 def test_get_joke_chuck_type():
+    """
+    Assert status code 200 in fetching Chuck jokes
+    """
     response = client.get("/joke/chuck", headers={"Accept": "application/json"})
     assert response.status_code == 200
 
 
 def test_get_joke_dad_type():
+    """
+    Assert status code 200 in fetching Dad jokes
+    """
     response = client.get("/joke/dad", headers={"Accept": "application/json"})
     assert response.status_code == 200
 
@@ -32,12 +44,18 @@ def test_get_joke_dad_type():
 
 
 def test_lcm_good():
+    """
+    Assert status code 200 and correct LCM
+    """
     response = client.get("/lcm?numbers=1,2,3", headers={"Accept": "application/json"})
     assert response.status_code == 200
     assert response.json() == {"lcm": 6}
 
 
 def test_lcm_bad_pass_wrong_separator():
+    """
+    Assert status code 400 and incorrect separator in integer list
+    """
     response = client.get("/lcm?numbers=1,2-3", headers={"Accept": "application/json"})
     assert response.status_code == 400
     assert response.json() == {
@@ -46,6 +64,9 @@ def test_lcm_bad_pass_wrong_separator():
 
 
 def test_lcm_bad_pass_letters():
+    """
+    Assert status code 400 and incorrect items in integer list
+    """
     response = client.get("/lcm?numbers=1,2,A", headers={"Accept": "application/json"})
     assert response.status_code == 400
     assert response.json() == {
@@ -54,6 +75,9 @@ def test_lcm_bad_pass_letters():
 
 
 def test_plus_one_good():
+    """
+    Assert status code 200 and correct addition
+    """
     response = client.get(
         "/plus-one?number=123",
         headers={"Accept": "application/json"},
@@ -63,6 +87,9 @@ def test_plus_one_good():
 
 
 def test_plus_one_bad_format():
+    """
+    Assert status code 422 for incorrect input (only integers)
+    """
     response = client.get(
         "/plus-one?number=bad", headers={"Accept": "application/json"}
     )
